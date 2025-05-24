dc742339f97507858bd890378d27aa1ea44d956e
Adriana Albuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2025-05-24 17:40:34 -0300

    feat(models): adiciona model User com campos e constraints

---
+++ app/database/models/__init__.py
@@ -0,0 +1,4 @@
+from .user_model import User
+
+__all__ = ["User"]
+##__all__ = ["User", "Client", "Product", "Order", "Order_itens"] ## nome das classes criadas no model
---
+++ app/database/models/user_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from database.engine import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(100), nullable=False)
+    user_email = Column(String(200), unique=True, nullable=False)
+    user_tipo = Column(String(1), nullable=False)
+    user_senha = Column(String(255), nullable=False)
