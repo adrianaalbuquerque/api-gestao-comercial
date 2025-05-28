bcfef9ee0c64759a6f762a4332926d67388fcf5a
Adriana Albuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2025-05-28 11:27:31 -0300

    feat(main): inclui clients_router com prefixo /client

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from typing import Union
 from fastapi import FastAPI
 from routers import auth_router
+from routers import clients_router
 
 app = FastAPI()
 app.include_router(auth_router, prefix="/auth")
+app.include_router(clients_router, prefix="/client")
 
 @app.get("/")
 def read_root():
