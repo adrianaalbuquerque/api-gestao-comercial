9a9294b68188f48939285c5b4c630fbafa1f2cdb
Adriana Albuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2025-06-05 16:07:59 -0300

    feat: cria prefixo de products

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
-from typing import Union
 from fastapi import FastAPI
-from routers import auth_router
-from routers import clients_router
+from routers import auth_router, clients_router, products_router
 
 app = FastAPI()
 app.include_router(auth_router, prefix="/auth")
 app.include_router(clients_router, prefix="/clients")
+app.include_router(products_router, prefix="/products")
 
 @app.get("/")
 def read_root():
