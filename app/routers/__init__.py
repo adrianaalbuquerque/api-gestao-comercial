a789103f52618359f2af5e3acb822e6fe2390a7e
Adriana Albuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2025-06-05 10:26:06 -0300

    chore(routers): adiciona products_router no __init__.py para simplificar os imports

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,4 +1,5 @@
 from .auth_router import router as auth_router
 from .clients_router import router as clients_router
+from .products_router import router as products_router
 
-__all__ = ["auth_router", "clients_router"]
+__all__ = ["auth_router", "clients_router", "products_router"]
