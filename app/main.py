f283066825dc4a58705ce68551a547cca0098c1d
Adriana Albuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2025-07-03 20:23:16 -0300

    feat: adição de trecho do uvicorn para debug e prefixo da rota de orders

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
-from routers import auth_router, clients_router, products_router
+import uvicorn
+from routers import auth_router, clients_router, products_router, orders_router
 
 app = FastAPI()
 app.include_router(auth_router, prefix="/auth")
 app.include_router(clients_router, prefix="/clients")
 app.include_router(products_router, prefix="/products")
+app.include_router(orders_router, prefix="/orders")
 
 @app.get("/")
 def read_root():
     return {"mensagem": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
