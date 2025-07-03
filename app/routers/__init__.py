e397fdc0ccc9b865296eb04d9204e02fd48073d6
Adriana Albuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2025-07-03 20:19:09 -0300

    chore: adiciona método ao __init__.py de routers para facilitar importações

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,5 +1,6 @@
 from .auth_router import router as auth_router
 from .clients_router import router as clients_router
 from .products_router import router as products_router
+from .orders_router import router as orders_router
 
-__all__ = ["auth_router", "clients_router", "products_router"]
+__all__ = ["auth_router", "clients_router", "products_router", "orders_router"]
