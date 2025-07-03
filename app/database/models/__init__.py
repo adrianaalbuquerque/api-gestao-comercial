2aa16b362b073e3ff83d43036b8dd45acda7556d
Adriana Albuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2025-07-03 20:15:32 -0300

    chore: adiciona classes ao __init__.py de models para facilitar importações

--- app/database/models/__init__.py
+++ app/database/models/__init__.py
@@ -1,5 +1,7 @@
 from .user_model import User
 from .client_model import Client
 from .product_model import ProductModel
+from .order_model import OrderModel
+from .orders_products import OrdersProducts
 
-__all__ = ["User", "Client", "ProductModel"]
+__all__ = ["User", "Client", "ProductModel", "OrderModel", "OrdersProducts"]
