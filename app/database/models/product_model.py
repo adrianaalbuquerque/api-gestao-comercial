e6a2c531927cf5e5f2c1b1437dd7d008356b1e87
Adriana Albuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2025-07-03 20:23:38 -0300

    refactor: remoção de 'inicial' do estoque atual

--- app/database/models/product_model.py
+++ app/database/models/product_model.py
@@ -10,9 +10,6 @@ class ProductModel(Base):
     valor_venda = Column(Numeric(10, 2), nullable=False)  
     codigo_barras = Column(String(50), unique=True, nullable=False)
     secao = Column(String(100), nullable=True) 
-    estoque_inicial = Column(Integer, nullable=False, default=0)
+    estoque = Column(Integer, nullable=False, default=0)
     data_validade = Column(Date, nullable=True)
     imagens = Column(Text, nullable=True)
-
-    usuario_id = Column(Integer, ForeignKey("users.id"))
-    usuario = relationship("User", back_populates="produtos")
